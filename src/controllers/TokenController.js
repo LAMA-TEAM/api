b2740a1a3265a62e801b16ff68d7be72604cbd76
macsimtv <maximelecouturier@gmail.com>
2022-11-17 17:08:01 +0100

    [Fix]

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -15,14 +15,17 @@ class TokenController {
     }
 
     static async refreshToken(req, res) {
-        const { token } = req.body;
+        const { token } = req.params;
 
         try {
-            const isTokenValid = await Token.find({ token, isActive: true, user: req.user._id });
+            const isTokenValid = await Token.find({ _id: token, isActive: true, user: req.user._id });
 
             if (isTokenValid.length <= 0) return res.status(401).json({ success: false, message: 'Invalid token' });
 
+            
             const updateToken = await Token.findByIdAndUpdate(isTokenValid[0]._id, { useLeft: 50 });
+            
+            updateToken.save();
 
             return res.status(200).json({ success: true, message: 'Token refreshed' });
         } catch (error) {
