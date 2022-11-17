ac16b5a13ff9c2d5cb5250d3b6b4407379a0c7f1
macsimtv <maximelecouturier@gmail.com>
2022-11-17 16:34:08 +0100

    [Add] Token revoke

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -29,6 +29,22 @@ class TokenController {
             return res.status(500).json({ success: false, message: 'Server error' });
         }
     }
+
+    static async destroy(req, res) {
+        const { token } = req.body;
+
+        try {
+            const isTokenValid = await Token.find({ token, isActive: true, user: req.user});
+
+            if (isTokenValid.length <= 0) return res.status(401).json({ success: false, message: 'Invalid token' });
+
+            const updateToken = await Token.findByIdAndUpdate(isTokenValid[0]._id, { isActive: false });
+
+            return res.status(200).json({ success: true, message: 'Token destroyed' });
+        } catch (error) {
+            return res.status(500).json({ success: false, message: 'Server error' });
+        }
+    }
 }
 
 module.exports = TokenController;
